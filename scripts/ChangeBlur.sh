b71c50d402a3601a88ce2279f171a25ff6b3785d
Ajay Barman <barmanjiaj@gmail.com>
2025-01-09 23:12:55 +0530

    Update basic things

--- scripts/ChangeBlur.sh
+++ scripts/ChangeBlur.sh
@@ -6,12 +6,12 @@ notif="$HOME/.config/swaync/images/bell.png"
 
 STATE=$(hyprctl -j getoption decoration:blur:passes | jq ".int")
 
-if [ "${STATE}" == "2" ]; then
-	hyprctl keyword decoration:blur:size 2
-	hyprctl keyword decoration:blur:passes 1
- 	notify-send -e -u low -i "$notif" "Less blur"
+if [ "${STATE}" == "0" ]; then
+	hyprctl keyword decoration:blur:size 4
+	hyprctl keyword decoration:blur:passes 4
+ 	notify-send -e -u low -i "$notif" "Blur"
 else
-	hyprctl keyword decoration:blur:size 5
-	hyprctl keyword decoration:blur:passes 2
-  	notify-send -e -u low -i "$notif" "Normal blur"
+	hyprctl keyword decoration:blur:size 0
+	hyprctl keyword decoration:blur:passes 0
+  	notify-send -e -u low -i "$notif" "Transparent"
 fi
